f94f523910806979b8b2f76a10c07be5c03b43c1
Auriane Dubois <auriane.dubois@outlook.fr>
2019-04-12 16:13:38 +0200

    [src] adding visuel and moving gestion crise to an application

---
+++ src/Visuel.java
@@ -0,0 +1,41 @@
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.application.*;
+import javafx.event.EventHandler;
+import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.*;
+import javafx.stage.*;
+import javafx.scene.text.TextAlignment;
+
+public class Visuel extends Application {
+	
+	public void start(Stage stage) {
+		StackPane root = new StackPane();
+        Scene scene = new Scene(root);
+        
+        HBox layout= new HBox();
+        Text debut = new Text("Vous êtes Olivier Chandessu, (age, situation pro, poste) au sein de la compagnie aérienne AirEnac.\n"
+        		+ "Cette compagnie se situe dans le milieu de l’aviation d’affaire et compte beaucoup d’entreprises telles que X, Y et Z comme clients.\r\n" + 
+        		"Alors que les attaques informatiques se multiplient, vous décidez d’investir dans divers moyens afin de protéger votre entreprise.\nVous avez pour cela un budget de XXX euros.\r\n" + 
+        		"Toutes les actions requièrent du temps pour être mises en place, et certaines actions ne peuvent pas être menées en même temps,\nchoisissez donc judicieusement.\r\n" + 
+        		"");
+        debut.setTextAlignment(TextAlignment.JUSTIFY);
+        
+        layout.getChildren().add(debut);       
+        root.getChildren().add(layout);
+     
+        stage.setTitle("Jeu de Gestion de crise ssi");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+	}
+
+	
+}
--- src/jeu_gestion_de_crise.java
+++ src/jeu_gestion_de_crise.java
@@ -1,9 +1,46 @@
+import javafx.application.*;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
 
-public class jeu_gestion_de_crise {
+public class jeu_gestion_de_crise extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		launch(args);
 	}
+	
+	public void start(Stage stage) {
+		StackPane root = new StackPane();
+        Scene scene = new Scene(root);
+        
+        VBox layout= new VBox();
+        Text debut = new Text("Bienvenue sur le jeu de gestion de crise ssi");
+        debut.setTextAlignment(TextAlignment.JUSTIFY);
+        
+        layout.getChildren().add(debut);
+        
+        Button bouton = new Button("start");
+        EventHandler<ActionEvent> quitHandler = quitEvent -> {
+            System.exit(0);
+        };
+        bouton.setOnAction(quitHandler);
+        layout.getChildren().add(bouton);
+        
+        
+        root.getChildren().add(layout);
 
+        
+        stage.setTitle("Jeu de Gestion de crise ssi");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+	}
 }
