2974484d7d2cbfe1c49ca198dfb7522705c747d3
Adrien <adrien.caillet@alumni.enac.fr>
2019-04-12 13:17:30 +0200

    [ssi] created the company class

---
+++ src/Entreprise.java
@@ -0,0 +1,24 @@
+
+
+public class Entreprise {
+	private String name;
+	private double reputationClient;
+	private double argent;
+	private double reputationPublic;
+	private double infrastructures;
+	private double donnees;
+	
+	
+	
+	public Entreprise(String name, double reputationClient, double argent, double reputationPublic, double infrastructures, double donnees) {
+		this.name = name;
+		this.reputationClient = reputationClient;
+		this.argent = argent;
+		this.reputationPublic = reputationPublic;
+		this.infrastructures = infrastructures;
+		this.donnees = donnees;
+	}
+	
+	
+	
+}
